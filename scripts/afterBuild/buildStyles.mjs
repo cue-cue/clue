f634a9e96ce5c415bc83eeea7019993c0c506964
NikitaPalmDev <nikitafedorov7@yandex.ru>
2023-10-04 16:52:51 +0400

    feat: add postCss and autoprefixer

--- scripts/afterBuild/buildStyles.mjs
+++ scripts/afterBuild/buildStyles.mjs
@@ -1,29 +1,37 @@
 import config from "../config.mjs";
+import postcssConfig from '../../postcss.config.js'
 import {globSync} from 'glob'
 import fs from 'fs'
 import * as sass from 'sass'
+import postcss from "postcss";
 
 const path = `./${config.buildDir}/packages/styles/**/index.{sass,scss}`
 
-const run = () => {
+const run = async () => {
     const paths = globSync(path)
     paths.map(path => {
         console.log(`start ${path}`)
+
 
         const styles = sass.compile(path, {
             style: 'compressed',
         })
         
-        fs.writeFileSync(path, styles.css)
+        let css = styles.css
+        
+        fs.writeFileSync(path, css)
         
         return {
             path,
-            styles
+            css
         }
-    }).forEach(({path}) => {
+    }).forEach(({path, css}) => {
         const newPath = path.replace(/\.[^.]+$/, '.css')
         fs.rename(path, newPath, (err) => {
             if (err) throw err
+            postcss(postcssConfig.plugins).process(css).then(res => {
+                fs.writeFileSync(newPath, res.css)
+            })
             console.log(`rename ${path} => ${newPath}`)
         })
     })
