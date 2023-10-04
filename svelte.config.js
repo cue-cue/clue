d47dab22b2eee1f2979bfe0a4e71cdcadc4074ca
NikitaPalmDev <nikitafedorov7@yandex.ru>
2023-10-04 16:34:09 +0400

    feat: autoprefix

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,9 @@ const config = {
 			return `clue-${t.hash(t.css + t.name + t.filename)}`
 		},
 	},
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess()
+	],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
