fc9b430bb4943d2a32bdc126fed1a49d92d60b7f
NikitaPalmDev <nikitafedorov7@yandex.ru>
2023-10-04 19:12:54 +0400

    feat: update perf

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-    
+	import { navigating } from "$app/stores";
 </script>
 
-<h2 style="text-align: center;">Go to</h2>
+<h2 style="text-align: center;">Go to {$navigating?.to?.params?.group ? `${$navigating?.to?.params?.group}...` : ''}</h2>
