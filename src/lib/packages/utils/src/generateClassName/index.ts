43262bdf1cb44e955da37a2d818028b480b68208
NikitaPalmDev <nikitafedorov7@yandex.ru>
2023-10-06 00:08:01 +0400

    fix

--- src/lib/packages/utils/src/generateClassName/index.ts
+++ src/lib/packages/utils/src/generateClassName/index.ts
@@ -1,5 +1,5 @@
 import { get } from "svelte/store";
 import { camelize } from "../index.js";
-import { config } from "$lib/packages/config.js";
+import { config } from "../../../config.js";
 
 export const generateClassName = (className:string, prefix = get(config).prefix):string => camelize(`${prefix} ${className.replace(prefix, '')}`)
